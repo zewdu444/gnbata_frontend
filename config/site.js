eeecfec730820a5b12c7184dea340a8a3747f45c
zewdu444 <zewdu444@gmail.com>
2024-12-28 11:49:01 +0300

    add basic setup file for gnbata

---
+++ config/site.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "DashTail Next js Admin Template",
+  description: null,
+  theme: "red",
+  // yellow, blue, orange, rose, red, neutral, gray, stone, slate, zinc
+  layout: "vertical",
+  // semi-box, horizontal, vertical
+  hideSideBar: false,
+  sidebarType: "popover",
+  // popover, classic, module
+  sidebarColor: null,
+  navbarType: "sticky",
+  // sticky, floating, static
+  footerType: "static",
+  // sticky,  static, hidden
+  sidebarBg: "none",
+  radius: 0.5,
+};
